390bdb7184c7d1b043a3eee4404e94a6e5a31b3a
Jack Plane <jgplane@gmail.com>
2019-03-21 17:02:44 -0700

    Add test csv file. Add first test. Add notes on parsing file

--- spec/clippings_pluck_spec.rb
+++ spec/clippings_pluck_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "csv"
 
 RSpec.describe ClippingsPluck do
   it "has a version number" do
@@ -40,6 +41,28 @@ RSpec.describe ClippingsPluck do
     it 'parses assigns notes to candidates' do
       candidates = @plucker.run(clipping_data)
       expect(candidates.first[:note]).to eq('This is a note')
+    end
+  end
+
+  describe 'CsvPlucker' do
+    before :all do
+      ROOT = File.dirname __FILE__
+      File.open("#{ROOT}/clippings.csv") do |f|
+        @string = f.read
+      end
+
+      @csv_plucker = ClippingsPluck::CsvPlucker.new
+      @candidates = @csv_plucker.run(@string)
+    end
+
+    # Notes for implementation:
+    #
+    # SEPARATOR = "----------------------------------------------\t\t\t\r\t\t\t\r"
+    # metadata, clippings = @string.split(SEPARATOR)
+    # CSV.parse(clippings, headers: true, col_sep: "\t")
+    #
+    it 'parses the correct book title' do
+      expect(@candidates.first[:book_title]).to eq("Book Title")
     end
   end
 end
